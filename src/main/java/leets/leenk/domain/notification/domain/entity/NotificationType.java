dc1121d5f4bebc8c5377f6e925e98b4061a1ecd1
hyxklee <ewgt1234@naver.com>
2025-07-15 19:06:01 +0900

    refactor: 제목 통일

--- src/main/java/leets/leenk/domain/notification/domain/entity/NotificationType.java
+++ src/main/java/leets/leenk/domain/notification/domain/entity/NotificationType.java
@@ -6,10 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationType {
-    FEED_TAG("피드 태그", "%s이 나를 함께한 사람에 추가했어"),
-    NEW_FEED("새로운 피드", "새로운 게시글을 확인해 봐"),
-    FEED_FIRST_REACTION("새로운 공감", "내가 쓴 피드에 좋아요를 받았어"),
-    FEED_REACTION_COUNT("누적 공감", "내가 쓴 피드에 좋아요를 %d개 받았어");
+
+    FEED_TAG("Leenk", "%s이 나를 함께한 사람에 추가했어"),
+    NEW_FEED("Leenk", "새로운 게시글을 확인해 봐"),
+    FEED_FIRST_REACTION("Leenk", "내가 쓴 피드에 좋아요를 받았어"),
+    FEED_REACTION_COUNT("Leenk", "내가 쓴 피드에 좋아요를 %d개 받았어");
 
 
     private final String title;
