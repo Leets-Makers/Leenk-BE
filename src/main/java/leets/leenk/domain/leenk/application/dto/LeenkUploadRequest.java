7b4e3217996de31162e8c10a302a44cdf7143b6c
huncozyboy <leejh5838@naver.com>
2025-08-01 07:56:49 +0900

    refactor : 링크 업로드 요청 dto 유효성 검사 변경

--- src/main/java/leets/leenk/domain/leenk/application/dto/LeenkUploadRequest.java
+++ src/main/java/leets/leenk/domain/leenk/application/dto/LeenkUploadRequest.java
@@ -1,8 +1,10 @@
 package leets.leenk.domain.leenk.application.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -14,16 +16,19 @@ public record LeenkUploadRequest(
         @NotNull
         String place,
 
-        @NotNull @Schema(description = "링크 일시")
+        @NotNull
+        @Schema(description = "링크 일시")
         LocalDateTime meetingTime,
 
-        @NotNull @Min(value = 3, message = "모집 인원은 최소 3명이어야 합니다")
+        @NotNull
+        @Min(value = 3, message = "모집 인원은 최소 3명이어야 합니다")
+        @Max(value = 99, message = "모집 인원은 최대 99명까지 가능 합니다")
         Long maxParticipants,
 
         @NotNull
         String content,
 
-        @NotNull
+        @Size(max = 5, message = "이미지는 최대 5장까지 업로드할 수 있습니다")
         List<String> imageUrls
 ) {
 }
