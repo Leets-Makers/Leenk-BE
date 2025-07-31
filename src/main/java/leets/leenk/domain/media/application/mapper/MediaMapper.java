fd5ba20f921613f906018adcbd3f01d2da15cce4
huncozyboy <leejh5838@naver.com>
2025-07-31 16:12:34 +0900

    feat : toMedia로 링크 매핑 추가

--- src/main/java/leets/leenk/domain/media/application/mapper/MediaMapper.java
+++ src/main/java/leets/leenk/domain/media/application/mapper/MediaMapper.java
@@ -1,8 +1,10 @@
 package leets.leenk.domain.media.application.mapper;
 
 import leets.leenk.domain.feed.domain.entity.Feed;
+import leets.leenk.domain.leenk.domain.entity.Leenk;
 import leets.leenk.domain.media.application.dto.request.FeedMediaRequest;
 import leets.leenk.domain.media.domain.entity.Media;
+import leets.leenk.domain.media.domain.entity.enums.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +16,15 @@ public class MediaMapper {
                 .position(request.position())
                 .mediaUrl(request.mediaUrl())
                 .mediaType(request.mediaType())
+                .build();
+    }
+
+    public Media toMedia(Leenk leenk, String url, int position) {
+        return Media.builder()
+                .leenk(leenk)
+                .mediaUrl(url)
+                .mediaType(MediaType.IMAGE)
+                .position(position)
                 .build();
     }
 }
