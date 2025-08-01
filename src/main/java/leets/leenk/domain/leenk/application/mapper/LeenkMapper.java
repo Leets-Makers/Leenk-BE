b9af955f819345b793a0456bc3bc1bace33aeed6
huncozyboy <leejh5838@naver.com>
2025-08-01 09:09:48 +0900

    feat : 링크 리스트 매핑 로직 추가

--- src/main/java/leets/leenk/domain/leenk/application/mapper/LeenkMapper.java
+++ src/main/java/leets/leenk/domain/leenk/application/mapper/LeenkMapper.java
@@ -1,9 +1,16 @@
 package leets.leenk.domain.leenk.application.mapper;
 
-import leets.leenk.domain.leenk.application.dto.LeenkUploadRequest;
+import java.util.List;
+import java.util.Map;
+import leets.leenk.domain.leenk.application.dto.request.LeenkUploadRequest;
+import leets.leenk.domain.leenk.application.dto.response.LeenkListResponse;
+import leets.leenk.domain.leenk.application.dto.response.LeenkResponse;
 import leets.leenk.domain.leenk.domain.entity.Leenk;
 import leets.leenk.domain.leenk.domain.entity.Location;
+import leets.leenk.domain.media.domain.entity.Media;
 import leets.leenk.domain.user.domain.entity.User;
+import leets.leenk.global.common.dto.PageableMapperUtil;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +24,43 @@ public class LeenkMapper {
                 .content(request.content())
                 .startTime(request.meetingTime())
                 .maxParticipants(request.maxParticipants())
+                .build();
+    }
+
+    public LeenkListResponse toLeenkListResponse(Slice<Leenk> slice, Map<Long, List<Media>> mediaMap) {
+        List<LeenkResponse> responses = toLeenkResponses(slice.getContent(), mediaMap);
+
+        return LeenkListResponse.builder()
+                .leenks(responses)
+                .pageable(PageableMapperUtil.from(slice))
+                .build();
+    }
+
+    private List<LeenkResponse> toLeenkResponses(List<Leenk> leenks, Map<Long, List<Media>> mediaMap) {
+        return leenks.stream()
+                .map(leenk -> {
+                    List<Media> medias = mediaMap.getOrDefault(leenk.getId(), List.of());
+                    Media representative = medias.stream().findFirst()
+                            .orElse(null);
+
+                    return toLeenkResponse(leenk, representative);
+                })
+                .toList();
+    }
+
+    private LeenkResponse toLeenkResponse(Leenk leenk, Media representative) {
+        String imageUrl = (representative != null) ? representative.getMediaUrl() : null;
+
+        return LeenkResponse.builder()
+                .leenkId(leenk.getId())
+                .userId(leenk.getAuthor().getId())
+                .title(leenk.getTitle())
+                .currentParticipants(leenk.getCurrentParticipants())
+                .maxParticipants(leenk.getMaxParticipants())
+                .startTime(leenk.getStartTime())
+                .createdAt(leenk.getCreateDate())
+                .updatedAt(leenk.getUpdateDate())
+                .representativeImage(imageUrl)
                 .build();
     }
 }
