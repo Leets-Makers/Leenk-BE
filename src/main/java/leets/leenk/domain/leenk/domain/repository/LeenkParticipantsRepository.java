3421dd1adfece9360d98f485a8aad9676e47132a
huncozyboy <leejh5838@naver.com>
2025-08-01 09:32:56 +0900

    feat : 링크 모집글 참여자 리스트 반환에 사용할 findAllByLeenk 메서드 추가

--- src/main/java/leets/leenk/domain/leenk/domain/repository/LeenkParticipantsRepository.java
+++ src/main/java/leets/leenk/domain/leenk/domain/repository/LeenkParticipantsRepository.java
@@ -1,5 +1,7 @@
 package leets.leenk.domain.leenk.domain.repository;
 
+import java.util.List;
+import leets.leenk.domain.leenk.domain.entity.Leenk;
 import leets.leenk.domain.leenk.domain.entity.LeenkParticipants;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LeenkParticipantsRepository extends JpaRepository<LeenkParticipants, Long> {
 
+    List<LeenkParticipants> findAllByLeenk(Leenk leenk);
 }
