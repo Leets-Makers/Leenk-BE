cd3e5b505e37f84d0b9ea302484431d5e7926c05
huncozyboy <leejh5838@naver.com>
2025-08-01 10:59:46 +0900

    refactor : 링크 내보내기 기능 예외처리 구현

--- src/main/java/leets/leenk/domain/leenk/application/exception/ErrorCode.java
+++ src/main/java/leets/leenk/domain/leenk/application/exception/ErrorCode.java
@@ -16,7 +16,9 @@ public enum ErrorCode implements ErrorCodeInterface {
     LEENK_ALREADY_PARTICIPATED(2403, HttpStatus.BAD_REQUEST, "이미 참여한 모집글입니다."),
     LEENK_MAX_PARTICIPANTS_EXCEEDED(2404, HttpStatus.BAD_REQUEST, "이미 모집글의 최대 참여 인원을 초과했습니다."),
     LEENK_NOT_OWNER(2405, HttpStatus.FORBIDDEN, "해당 모집글의 작성자가(방장) 아닙니다."),
-    LEENK_ALREADY_CLOSED(2406, HttpStatus.BAD_REQUEST, "이미 마감된 모집글입니다.");
+    LEENK_ALREADY_CLOSED(2406, HttpStatus.BAD_REQUEST, "이미 마감된 모집글입니다."),
+    LEENK_CANNOT_KICK_SELF(2407, HttpStatus.BAD_REQUEST, "방장은 자신을 내보낼 수 없습니다."),
+    LEENK_PARTICIPANT_NOT_FOUND(2408, HttpStatus.NOT_FOUND, "해당 모집글에 참여하지 않은 사용자입니다.");
 
     private final int code;
     private final HttpStatus status;
