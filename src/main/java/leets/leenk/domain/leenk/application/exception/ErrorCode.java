6fd7005036400251a714ab54709b273904d69f96
huncozyboy <leejh5838@naver.com>
2025-08-09 03:18:27 +0900

    feat : 방장이 떠날시 예외처리 추가

--- src/main/java/leets/leenk/domain/leenk/application/exception/ErrorCode.java
+++ src/main/java/leets/leenk/domain/leenk/application/exception/ErrorCode.java
@@ -18,7 +18,8 @@ public enum ErrorCode implements ErrorCodeInterface {
     LEENK_NOT_OWNER(2405, HttpStatus.FORBIDDEN, "해당 링크의 작성자가(방장) 아닙니다."),
     LEENK_ALREADY_CLOSED(2406, HttpStatus.BAD_REQUEST, "이미 마감된 링크입니다."),
     LEENK_CANNOT_KICK_SELF(2407, HttpStatus.BAD_REQUEST, "방장은 자신을 내보낼 수 없습니다."),
-    LEENK_PARTICIPANT_NOT_FOUND(2408, HttpStatus.NOT_FOUND, "해당 링크에 참여하지 않은 사용자입니다.");
+    LEENK_PARTICIPANT_NOT_FOUND(2408, HttpStatus.NOT_FOUND, "해당 링크에 참여하지 않은 사용자입니다."),
+    LEENK_CANNOT_LEAVE_AS_HOST(2409, HttpStatus.BAD_REQUEST, "방장은 링크를 떠날 수 없습니다.");
 
     private final int code;
     private final HttpStatus status;
