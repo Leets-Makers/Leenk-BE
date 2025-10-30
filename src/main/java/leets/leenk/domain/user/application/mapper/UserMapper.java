b5914a6a7236908225f3fd4895b2f57b87c7ceb8
1winhyun <jimking1@naver.com>
2025-10-30 21:36:41 +0900

    feat: UserMapper 생일 추가

--- src/main/java/leets/leenk/domain/user/application/mapper/UserMapper.java
+++ src/main/java/leets/leenk/domain/user/application/mapper/UserMapper.java
@@ -14,6 +14,7 @@ public class UserMapper {
                 .name(user.getName())
                 .cardinal(user.getCardinal())
                 .profileImage(user.getProfileImage())
+                .birthday(user.getBirthday())
                 .introduction(user.getIntroduction())
                 .kakaoTalkId(user.getKakaoTalkId())
                 .introduction(user.getIntroduction())
