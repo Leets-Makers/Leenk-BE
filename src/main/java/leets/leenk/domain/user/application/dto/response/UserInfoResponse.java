a574ea65a3c2e5179f45a4249127ad30dbc31825
1winhyun <jimking1@naver.com>
2025-10-30 21:34:39 +0900

    feat: UserInfoResponse 생일 추가

--- src/main/java/leets/leenk/domain/user/application/dto/response/UserInfoResponse.java
+++ src/main/java/leets/leenk/domain/user/application/dto/response/UserInfoResponse.java
@@ -3,6 +3,8 @@ package leets.leenk.domain.user.application.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserInfoResponse(
@@ -10,6 +12,7 @@ public record UserInfoResponse(
         String name,
         int cardinal,
         String profileImage,
+        LocalDate birthday,
         String kakaoTalkId,
         String introduction,
         String mbti
