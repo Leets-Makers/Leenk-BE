3de340e9abe9657473a24532ec2444b5f7493e96
1winhyun <jimking1@naver.com>
2025-11-01 00:13:52 +0900

    refactor: ReactionUserResponse 오르 생일인지에 대한 응답값 추가

--- src/main/java/leets/leenk/domain/feed/application/dto/response/ReactionUserResponse.java
+++ src/main/java/leets/leenk/domain/feed/application/dto/response/ReactionUserResponse.java
@@ -16,6 +16,9 @@ public record ReactionUserResponse(
         @Schema(description = "사용자 이름", example = "이강혁")
         String name,
 
+        @Schema(description = "사용자 오늘 생일 여부", example = "true")
+        Boolean isUserBirthdayToday,
+
         @Schema(description = "사용자별 공감 개수", example = "10")
         long reactionCount
 ) {
--- src/main/java/leets/leenk/domain/feed/application/mapper/ReactionMapper.java
+++ src/main/java/leets/leenk/domain/feed/application/mapper/ReactionMapper.java
@@ -1,19 +1,24 @@
 package leets.leenk.domain.feed.application.mapper;
 
+import leets.leenk.domain.birthday.application.util.BirthdayChecker;
 import leets.leenk.domain.feed.application.dto.response.ReactionUserResponse;
 import leets.leenk.domain.feed.domain.entity.Feed;
 import leets.leenk.domain.feed.domain.entity.Reaction;
 import leets.leenk.domain.user.domain.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ReactionMapper {
+    private final BirthdayChecker birthdayChecker;
 
     public ReactionUserResponse toResponse(Reaction reaction) {
         return ReactionUserResponse.builder()
                 .userId(reaction.getUser().getId())
                 .profileImage(reaction.getUser().getThumbnail())
                 .name(reaction.getUser().getName())
+                .isUserBirthdayToday(birthdayChecker.isUserBirthdayToday(reaction.getUser()))
                 .reactionCount(reaction.getReactionCount())
                 .build();
     }
