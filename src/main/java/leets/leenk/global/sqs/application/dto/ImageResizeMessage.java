3d5fea2979f9ec6c2f2c6be9364ced9b61ded0c8
soo0711 <k300hys@naver.com>
2025-10-31 17:26:14 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/main/java/leets/leenk/global/sqs/application/dto/ImageResizeMessage.java
+++ src/main/java/leets/leenk/global/sqs/application/dto/ImageResizeMessage.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import leets.leenk.domain.media.domain.entity.enums.DomainType;
 
 public record ImageResizeMessage(
-        @JsonProperty("domainType")
         DomainType domainType,
-        @JsonProperty("originalUrl")
         String originalUrl,
-        @JsonProperty("resizedUrl")
         String resizedUrl
 ) {
 }
