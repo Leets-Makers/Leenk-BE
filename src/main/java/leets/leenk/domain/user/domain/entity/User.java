5db50aa4d4bc5ce1c99ac0cd98fafe59c56422fb
jj <syeonie0526@gmail.com>
2025-11-02 23:03:22 +0900

    feat: User에 birthday 필드 추가

--- src/main/java/leets/leenk/domain/user/domain/entity/User.java
+++ src/main/java/leets/leenk/domain/user/domain/entity/User.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.ColumnDefault;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -37,6 +38,9 @@ public class User extends BaseEntity {
     private int cardinal;
 
     private String profileImage;
+
+    @Column
+    private LocalDate birthday;
 
     @Size(max = 4)
     @Column(length = 4)
