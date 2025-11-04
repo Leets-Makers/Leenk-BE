d311adcfc11033e37df1f61522c6f687df6fa0d7
1winhyun <jimking1@naver.com>
2025-11-05 00:06:58 +0900

    refactor: BirthdatUsersReposnse 수정

--- src/main/java/leets/leenk/domain/birthday/application/dto/response/BirthdayUsersResponse.java
+++ src/main/java/leets/leenk/domain/birthday/application/dto/response/BirthdayUsersResponse.java
@@ -1,13 +1,19 @@
 package leets.leenk.domain.birthday.application.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import leets.leenk.domain.user.application.dto.response.UserProfileResponse;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record BirthdayUsersResponse(
+        @Schema(description = "생일인 유저들")
         List<UserProfileResponse> users,
-        Long myBirthdayLettersCount
+
+        @Schema(description = "받은 편지 개수", example = "100")
+        Long myBirthdayLettersCounts
 ) {
 }
