e23f5b83892cf8ca4423d375987ac88c356ffc7e
1winhyun <jimking1@naver.com>
2025-11-05 00:27:56 +0900

    refactor: countMyReceivedLetters 메서드 BirthdayLettersGetService 내로 이동

--- src/main/java/leets/leenk/domain/birthday/application/usecase/BirthdayUsecase.java
+++ src/main/java/leets/leenk/domain/birthday/application/usecase/BirthdayUsecase.java
@@ -3,7 +3,7 @@ package leets.leenk.domain.birthday.application.usecase;
 import leets.leenk.domain.birthday.application.dto.response.BirthdayUsersResponse;
 import leets.leenk.domain.birthday.application.mapper.BirthdayMapper;
 import leets.leenk.domain.birthday.domain.service.BirthdayGetService;
-import leets.leenk.domain.birthday.domain.service.CountReceivedLettersService;
+import leets.leenk.domain.birthday.domain.service.BirthdayLettersGetService;
 import leets.leenk.domain.user.application.dto.response.UserProfileResponse;
 import leets.leenk.domain.user.application.mapper.UserProfileMapper;
 import leets.leenk.domain.user.domain.entity.User;
@@ -20,7 +20,7 @@ public class BirthdayUsecase {
     private final BirthdayMapper birthdayMapper;
     private final UserProfileMapper userProfileMapper;
     private final BirthdayGetService birthdayGetService;
-    private final CountReceivedLettersService countReceivedLettersService;
+    private final BirthdayLettersGetService birthdayLettersGetService;
 
     @Transactional(readOnly = true)
     public BirthdayUsersResponse getTodayBirthdayUsers(long loginUserId) {
@@ -36,7 +36,7 @@ public class BirthdayUsecase {
 
         Long myBirthdayCounts = null;
         if (amIInBirthday) {
-            myBirthdayCounts = countReceivedLettersService.countMyReceivedLetters(loginUserId, today);
+            myBirthdayCounts = birthdayLettersGetService.countMyReceivedLetters(loginUserId, today);
         }
 
         return birthdayMapper.toBirthdayUsersResponse(response, myBirthdayCounts);
--- src/main/java/leets/leenk/domain/birthday/domain/service/BirthdayLettersGetService.java
+++ src/main/java/leets/leenk/domain/birthday/domain/service/BirthdayLettersGetService.java
@@ -5,6 +5,8 @@ import leets.leenk.domain.birthday.domain.repository.BirthdayLetterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -14,5 +16,12 @@ public class BirthdayLettersGetService {
 
     public List<BirthdayLetter> getMyBirthdayLetters(long receiverId) {
         return birthdayLetterRepository.findAllByReceiverIdOrderByCreateDateDesc(receiverId);
+    }
+
+    public long countMyReceivedLetters(long receiverId, LocalDate date) {
+        LocalDateTime start = date.atStartOfDay();
+        LocalDateTime end = start.plusDays(1);
+
+        return birthdayLetterRepository.countByReceiverIdAndCreateDateBetween(receiverId, start, end);
     }
 }
