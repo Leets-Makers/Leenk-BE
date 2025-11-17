d47555d568e1547649e3c2a3dba6f0d4f94bec06
soo0711 <k300hys@naver.com>
2025-11-18 02:13:20 +0900

    feat: S3 클라이언트에 copy, delete 권한 부여

--- src/main/java/leets/leenk/global/config/AwsS3Config.java
+++ src/main/java/leets/leenk/global/config/AwsS3Config.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -27,6 +28,16 @@ public class AwsS3Config {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, accessSecret);
 
         return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, accessSecret);
+
+        return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
