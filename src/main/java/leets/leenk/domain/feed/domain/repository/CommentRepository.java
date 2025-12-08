0c58efd778b7db26d4528f28e39b0ee4ff6f64d5
1winhyun <jimking1@naver.com>
2025-12-09 01:21:38 +0900

    feat: 댓글 조회 repository 구현

--- src/main/java/leets/leenk/domain/feed/domain/repository/CommentRepository.java
+++ src/main/java/leets/leenk/domain/feed/domain/repository/CommentRepository.java
@@ -1,8 +1,11 @@
 package leets.leenk.domain.feed.domain.repository;
 
 import leets.leenk.domain.feed.domain.entity.Comment;
+import leets.leenk.domain.feed.domain.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+import java.util.List;
 
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByFeed(Feed feed);
 }
