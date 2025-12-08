c3f2865f0ba8a35befc23cc9c4a6356aa4f66d3d
1winhyun <jimking1@naver.com>
2025-12-08 23:04:50 +0900

    feat: Comment 엔티티 구현

---
+++ src/main/java/leets/leenk/domain/feed/domain/entity/Comment.java
@@ -0,0 +1,36 @@
+package leets.leenk.domain.feed.domain.entity;
+
+import jakarta.persistence.*;
+import leets.leenk.domain.user.domain.entity.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@SuperBuilder
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @Column(name = "comments")
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "feed_id", nullable = false, updatable = false)
+    private Feed feed;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
