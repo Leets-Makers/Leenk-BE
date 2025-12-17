a2b66968357cb52ad72d4fc2c7e4426bec6d5860
1winhyun <jimking1@naver.com>
2025-12-17 17:56:11 +0900

    feat: Feed 관련 단위테스트를 위한 fixture 구현

---
+++ src/test/java/leets/leenk/domain/feed/test/FeedTestFixture.java
@@ -0,0 +1,15 @@
+package leets.leenk.domain.feed.test;
+
+import leets.leenk.domain.feed.domain.entity.Feed;
+import leets.leenk.domain.user.domain.entity.User;
+
+public class FeedTestFixture {
+    static Feed createFeed(Long id, User author) {
+        return Feed.builder()
+                .id(id)
+                .user(author)
+                .description("desc")
+                .totalReactionCount(0L)
+                .build();
+    }
+}
