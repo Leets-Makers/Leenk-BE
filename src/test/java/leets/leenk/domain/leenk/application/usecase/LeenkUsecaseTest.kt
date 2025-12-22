f5db8cf97e424b7eebce6abe4a10749b8f2b408b
soo0711 <k300hys@naver.com>
2025-12-22 14:49:34 +0900

    refactor: 불필요한 의존성, 어노테이션 제거

--- src/test/java/leets/leenk/domain/leenk/application/usecase/LeenkUsecaseTest.kt
+++ src/test/java/leets/leenk/domain/leenk/application/usecase/LeenkUsecaseTest.kt
@@ -21,15 +21,12 @@ import leets.leenk.domain.user.domain.service.user.UserGetService
 import leets.leenk.domain.user.test.fixture.UserTestFixture
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.junit.jupiter.MockitoExtension
+import org.junit.jupiter.api.Test
 
 
-@ExtendWith(MockitoExtension::class)
 class LeenkUsecaseTest {
 
     private val userGetService: UserGetService = mockk()
