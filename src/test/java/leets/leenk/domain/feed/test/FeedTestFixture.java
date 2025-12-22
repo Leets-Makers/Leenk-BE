db084508f20ac03b5f3ef349416d46679b470550
1winhyun <jimking1@naver.com>
2025-12-22 14:26:04 +0900

    refactor: FeedTestFixture createFeed 접근제어자 변경

--- src/test/java/leets/leenk/domain/feed/test/FeedTestFixture.java
+++ src/test/java/leets/leenk/domain/feed/test/FeedTestFixture.java
@@ -4,7 +4,7 @@ import leets.leenk.domain.feed.domain.entity.Feed;
 import leets.leenk.domain.user.domain.entity.User;
 
 public class FeedTestFixture {
-    static Feed createFeed(Long id, User author) {
+    public static Feed createFeed(Long id, User author) {
         return Feed.builder()
                 .id(id)
                 .user(author)
