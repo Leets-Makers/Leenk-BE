b4dbc2722a447b0f18118486a3ed63203be8c2bf
hyxklee <ewgt1234@naver.com>
2026-01-06 17:19:24 +0900

    chore: 의존성 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "2.3.0"
     id("org.jetbrains.kotlin.plugin.spring") version "2.3.0"
     id("org.jetbrains.kotlin.plugin.jpa") version "2.3.0"
-    id("org.springframework.boot") version "3.5.0"
+    id("org.springframework.boot") version "3.5.9"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -38,7 +38,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
     // Swagger
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.15")
 
     // Lombok
     annotationProcessor("org.projectlombok:lombok")
@@ -54,17 +54,18 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
     testImplementation("org.testcontainers:mongodb")
 
     // Kotest
-    testImplementation("io.kotest:kotest-runner-junit5:6.0.7")
-    testImplementation("io.kotest:kotest-assertions-core:6.0.7")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
 
     // S3
-    implementation("software.amazon.awssdk:s3:2.31.54")
+    implementation("software.amazon.awssdk:s3:2.41.1")
 
     // AWS
     implementation("io.awspring.cloud:spring-cloud-aws-starter-sqs")
