879f5138ae6003738371a16230a33846858d4a9a
hyxklee <ewgt1234@naver.com>
2026-01-22 19:38:48 +0900

    refactor: 오타 수정

--- src/main/java/leets/leenk/domain/user/application/exception/UserErrorCode.java
+++ src/main/java/leets/leenk/domain/user/application/exception/UserErrorCode.java
@@ -18,7 +18,7 @@ public enum UserErrorCode implements ErrorCodeInterface {
     USER_SETTING_NOT_FOUND(2101, HttpStatus.NOT_FOUND, "존재하지 않는 사용자 설정입니다."),
 
     @ExplainError("이미 탈퇴 처리된 사용자 계정에 접근을 시도할 때 발생합니다.")
-    USER_ALREADY_Leave(2102, HttpStatus.BAD_REQUEST, "이미 탈퇴한 사용자입니다."),
+    USER_ALREADY_LEAVE(2102, HttpStatus.BAD_REQUEST, "이미 탈퇴한 사용자입니다."),
 
     @ExplainError("계정 복구 시 백업된 사용자 정보를 찾을 수 없을 때 발생합니다.")
     USER_BACKUP_INFO_ERROR(2103, HttpStatus.NOT_FOUND, "백업 정보를 찾을 수 없어 계정을 복구할 수 없습니다."),
