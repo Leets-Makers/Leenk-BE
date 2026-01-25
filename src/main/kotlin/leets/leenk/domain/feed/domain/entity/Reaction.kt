12cde39d85198afa1e0ea70f369f5bb4d48e2913
1winhyun <jimking1@naver.com>
2026-01-25 23:44:26 +0900

    feat: domain/feed 내 엔티티 마이그레이션

---
+++ src/main/kotlin/leets/leenk/domain/feed/domain/entity/Reaction.kt
@@ -0,0 +1,38 @@
+package leets.leenk.domain.feed.domain.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
+import leets.leenk.domain.user.domain.entity.User
+
+@Entity
+@Table(
+    name = "reactions",
+    uniqueConstraints = [
+        UniqueConstraint(columnNames = ["feed_id", "user_id"]),
+    ],
+)
+class Reaction(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    var reactionCount: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "feed_id", nullable = false, updatable = false)
+    val feed: Feed,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    val user: User,
+) {
+    fun increaseReactionCount(reactionCount: Long) {
+        this.reactionCount += reactionCount
+    }
+}
