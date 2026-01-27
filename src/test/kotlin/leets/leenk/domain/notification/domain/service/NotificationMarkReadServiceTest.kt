8ba4833b74697258fb6df54d5bc46ce2b3e87e28
jj <syeonie0526@gmail.com>
2026-01-27 22:01:30 +0900

    test: NotificationMarkReadService 테스트 코드 Kotest StringSpec 컨벤션으로 변경

--- src/test/kotlin/leets/leenk/domain/notification/domain/service/NotificationMarkReadServiceTest.kt
+++ src/test/kotlin/leets/leenk/domain/notification/domain/service/NotificationMarkReadServiceTest.kt
@@ -1,45 +1,35 @@
 package leets.leenk.domain.notification.domain.service
 
-import io.mockk.every
-import io.mockk.impl.annotations.InjectMockKs
-import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.extension.ExtendWith
+import io.kotest.core.spec.style.StringSpec
+
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
 import io.mockk.verify
 import leets.leenk.domain.notification.domain.repository.NotificationRepository
 import leets.leenk.domain.notification.test.fixture.NotificationFixture
 import leets.leenk.domain.user.test.fixture.UserFixture
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import java.util.Optional
 
-@ExtendWith(MockKExtension::class)
-internal class NotificationMarkReadServiceTest {
-    @MockK
-    private lateinit var notificationRepository: NotificationRepository
-    @InjectMockKs
-    private lateinit var notificationMarkReadService: NotificationMarkReadService;
+class NotificationMarkReadServiceTest : StringSpec({
+    val notificationRepository = mockk<NotificationRepository>()
+    val notificationMarkReadService = NotificationMarkReadService(notificationRepository)
 
-    @Test
-    fun markReadNotification() {
-        // given
-        val user = UserFixture.basicUser();
-        val notification = NotificationFixture.basicNotification();
+    "유효한 알림 ID로 읽음 처리 요청 시 알림의 읽음 상태가 true로 변경되어야 한다" {
+        // [Setup]
+        val user = UserFixture.basicUser()
+        val notification = NotificationFixture.basicNotification()
 
-        every {
-            notificationRepository.findById(notification.id)
-        } returns Optional.of(notification)
+        every { notificationRepository.findById(notification.id) } returns Optional.of(notification)
+        every { notificationRepository.save(notification) } returns notification
 
-        every {
-            notificationRepository.save(notification)
-        } returns notification
+        // [Execution]
+        notificationMarkReadService.markReadNotification(user, notification.id)
 
-        // when
-        notificationMarkReadService.markReadNotification(user, notification.id);
-
-        // then
-        assert(notification.isRead)
-        verify(exactly = 1) {
-            notificationRepository.save(notification)
-        }
+        // [Assertion]
+        notification.isRead shouldBe true
+        verify(exactly = 1) { notificationRepository.save(notification) }
     }
-}
+})
