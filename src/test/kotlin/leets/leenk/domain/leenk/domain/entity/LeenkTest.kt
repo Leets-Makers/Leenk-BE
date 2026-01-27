6507ee66e32050118393cc0d11f6e7800bafe046
soo0711 <k300hys@naver.com>
2026-01-27 13:59:48 +0900

    style: 불필요한 주석 삭제

--- src/test/kotlin/leets/leenk/domain/leenk/domain/entity/LeenkTest.kt
+++ src/test/kotlin/leets/leenk/domain/leenk/domain/entity/LeenkTest.kt
@@ -26,48 +26,36 @@ class LeenkTest :
             )
 
         "increaseCurrentParticipants 호출 시 현재 참여자 수가 1 증가한다" {
-            // Given : 현재 참여자가 2명인 모집 중인 링크
             val leenk = createLeenk()
             val initialParticipants = leenk.currentParticipants
 
-            // When : 현재 참여자 수를 1 증가시킨다
             leenk.increaseCurrentParticipants()
 
-            // Then : 현재 참여자 수가 1 증가했는지 확인한다
             leenk.currentParticipants shouldBe initialParticipants + 1
         }
 
         "decreaseCurrentParticipants 호출 시 현재 참여자 수가 1 감소한다" {
-            // Given: 현재 참여자가 2명인 모집 중인 링크
             val leenk = createLeenk()
             val initialParticipants = leenk.currentParticipants
 
-            // When: 현재 참여자 수를 1 감소 시킨다
             leenk.decreaseCurrentParticipants()
 
-            // Then: 현재 참여자 수가 1 감소했는지 확인한다
             leenk.currentParticipants shouldBe initialParticipants - 1
         }
 
         "changeStatusToClosed 호출 시 상태가 RECRUITING에서 CLOSED로 변경된다" {
-            // Given: RECRUITING 상태의 링크
             val leenk = createLeenk()
 
-            // When: 링크를 마감 처리한다
             leenk.changeStatusToClosed()
 
-            // Then: 링크 상태가 CLOSED로 변경됐는지 확인한다
             leenk.status shouldBe LeenkStatus.CLOSED
         }
 
         "changeStatusToFinished 호출 시 상태가 FINISHED로 변경된다" {
-            // Given : RECRUITING 상태의 링크
             val leenk = createLeenk()
 
-            // When : 링크를 종료 처리한다
             leenk.changeStatusToFinished()
 
-            // Then : 링크 상태가 FINISHED로 변경됐는지 확인한다
             leenk.status shouldBe LeenkStatus.FINISHED
         }
     })
