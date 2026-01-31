428c3c6c8c2de1b7dd6ce0cbc997a5141fdc06a0
1winhyun <jimking1@naver.com>
2026-01-31 23:37:08 +0900

    refactor: domain/feed 기존 자바 코드 삭제

--- src/main/java/leets/leenk/domain/feed/application/exception/FeedDeleteNotAllowedException.java
+++
@@ -1,9 +0,0 @@
-package leets.leenk.domain.feed.application.exception;
-
-import leets.leenk.global.common.exception.BaseException;
-
-public class FeedDeleteNotAllowedException extends BaseException {
-    public FeedDeleteNotAllowedException() {
-        super(FeedErrorCode.FEED_DELETE_NOT_ALLOWED);
-    }
-}
--- src/main/java/leets/leenk/domain/feed/application/exception/SelfReactionNotAllowedException.java
+++
@@ -1,9 +0,0 @@
-package leets.leenk.domain.feed.application.exception;
-
-import leets.leenk.global.common.exception.BaseException;
-
-public class SelfReactionNotAllowedException extends BaseException {
-    public SelfReactionNotAllowedException() {
-        super(FeedErrorCode.SELF_REACTION);
-    }
-}
