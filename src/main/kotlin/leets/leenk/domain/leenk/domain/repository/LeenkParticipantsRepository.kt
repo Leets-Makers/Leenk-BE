eab2235aa4564adffd9787128585f732d495ce1e
soo0711 <k300hys@naver.com>
2026-02-02 16:59:39 +0900

    refactor: LeenkParticipantsRepository 메서드 반환 타입을 Optional로 수정

--- src/main/kotlin/leets/leenk/domain/leenk/domain/repository/LeenkParticipantsRepository.kt
+++ src/main/kotlin/leets/leenk/domain/leenk/domain/repository/LeenkParticipantsRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface LeenkParticipantsRepository : JpaRepository<LeenkParticipants, Long> {
@@ -25,5 +26,5 @@ interface LeenkParticipantsRepository : JpaRepository<LeenkParticipants, Long> {
     fun findByLeenkIdAndParticipantId(
         leenkId: Long,
         participantId: Long,
-    ): LeenkParticipants?
+    ): Optional<LeenkParticipants>
 }
