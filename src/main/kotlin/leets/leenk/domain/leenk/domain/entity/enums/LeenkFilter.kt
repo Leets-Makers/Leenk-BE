4425bef3e6cab4711ee8b82e4379c664d915b57f
soo0711 <k300hys@naver.com>
2026-02-02 16:05:00 +0900

    refactor: domain/leenk 내 enums kotlin 문법으로 변환

--- src/main/kotlin/leets/leenk/domain/leenk/domain/entity/enums/LeenkFilter.kt
+++ src/main/kotlin/leets/leenk/domain/leenk/domain/entity/enums/LeenkFilter.kt
@@ -1,14 +1,9 @@
-package leets.leenk.domain.leenk.domain.entity.enums;
+package leets.leenk.domain.leenk.domain.entity.enums
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public enum LeenkFilter {
+enum class LeenkFilter(
+    val leenkStatus: LeenkStatus?,
+) {
     ALL(null),
     OPEN(LeenkStatus.RECRUITING),
-    CLOSED(LeenkStatus.CLOSED);
-
-    private final LeenkStatus leenkStatus;
+    CLOSED(LeenkStatus.CLOSED),
 }
--- src/main/kotlin/leets/leenk/domain/leenk/domain/entity/enums/LeenkStatus.kt
+++ src/main/kotlin/leets/leenk/domain/leenk/domain/entity/enums/LeenkStatus.kt
@@ -1,14 +1,9 @@
-package leets.leenk.domain.leenk.domain.entity.enums;
+package leets.leenk.domain.leenk.domain.entity.enums
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public enum LeenkStatus {
+enum class LeenkStatus(
+    val displayValue: String,
+) {
     RECRUITING("모집중"),
     CLOSED("모집 완료"),
-    FINISHED("모임 종료");
-
-    private final String displayValue;
+    FINISHED("모임 종료"),
 }
