bdf245afc9825afdef0b7dd3171183959e1ca4df
jj <syeonie0526@gmail.com>
2026-02-03 14:57:31 +0900

    refactor: NotificationFixture ktlint 포맷으로 변경

--- src/test/kotlin/leets/leenk/domain/notification/test/fixture/NotificationFixture.kt
+++ src/test/kotlin/leets/leenk/domain/notification/test/fixture/NotificationFixture.kt
@@ -3,13 +3,11 @@ package leets.leenk.domain.notification.test.fixture
 import leets.leenk.domain.notification.domain.entity.Notification
 
 object NotificationFixture {
-    fun basicNotification(
-
-    ): Notification {
-        return Notification.builder()
+    fun basicNotification(): Notification =
+        Notification
+            .builder()
             .id("100")
             .isRead(false)
             .userId(1L)
-            .build();
-    }
+            .build()
 }
