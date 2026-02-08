2e1147178535c1922c773aea00a9ff1332067d01
1winhyun <jimking1@naver.com>
2026-02-08 22:56:17 +0900

    feat: domain/birthday 내 mapper 마이그레이션

---
+++ src/main/kotlin/leets/leenk/domain/birthday/application/mapper/BirthdayLetterMapper.kt
@@ -0,0 +1,43 @@
+package leets.leenk.domain.birthday.application.mapper
+
+import leets.leenk.domain.birthday.application.dto.request.BirthdayLetterRequest
+import leets.leenk.domain.birthday.application.dto.response.MyBirthdayLettersResponse
+import leets.leenk.domain.birthday.domain.entity.BirthdayLetter
+import leets.leenk.domain.birthday.domain.entity.BirthdayLetterReadMark
+import leets.leenk.domain.user.application.mapper.UserProfileMapper
+import leets.leenk.domain.user.domain.entity.User
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+class BirthdayLetterMapper(
+    private val userProfileMapper: UserProfileMapper,
+) {
+    fun toBirthdayLetter(
+        sender: User,
+        receiver: User,
+        request: BirthdayLetterRequest,
+    ): BirthdayLetter =
+        BirthdayLetter(
+            sender = sender,
+            receiver = receiver,
+            message = request.message,
+        )
+
+    fun toMyBirthdayLettersResponse(birthdayLetter: BirthdayLetter): MyBirthdayLettersResponse =
+        MyBirthdayLettersResponse(
+            letterId = birthdayLetter.id!!,
+            author = userProfileMapper.toProfile(birthdayLetter.sender),
+            message = birthdayLetter.message,
+            createdAt = birthdayLetter.createDate,
+        )
+
+    fun toBirthdayLetterReadMark(
+        receiverId: Long,
+        now: LocalDateTime,
+    ): BirthdayLetterReadMark =
+        BirthdayLetterReadMark(
+            receiverId = receiverId,
+            lastReadAt = now,
+        )
+}
