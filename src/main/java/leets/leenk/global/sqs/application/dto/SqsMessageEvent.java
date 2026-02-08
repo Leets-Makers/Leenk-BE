7ba05359ea0f6e7af132c167c5c8773fbcb68944
jj <syeonie0526@gmail.com>
2026-02-09 01:28:05 +0900

    refactor: SqsMessageEvent 생성자 생성

--- src/main/java/leets/leenk/global/sqs/application/dto/SqsMessageEvent.java
+++ src/main/java/leets/leenk/global/sqs/application/dto/SqsMessageEvent.java
@@ -13,4 +13,12 @@ public class SqsMessageEvent {
     private final String path;
     private final Long id;
 
+    public SqsMessageEvent(String title, String content, String fcmToken, String path, Long id) {
+        this.title = title;
+        this.content = content;
+        this.fcmToken = fcmToken;
+        this.path = path;
+        this.id = id;
+    }
+
 }
