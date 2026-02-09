1f4fa724f2ae616ebe76075665cb02ced2d8d02e
jj <syeonie0526@gmail.com>
2026-02-09 17:02:25 +0900

    refactor: NotificationEntityContent 삭제

--- src/main/kotlin/leets/leenk/domain/notification/domain/entity/NotificationEntityContent.kt
+++
@@ -1,8 +0,0 @@
-package leets.leenk.domain.notification.domain.entity
-
-data class NotificationEntityContent(
-    val title: String,
-    val body: String,
-    val targetId: Long,
-    val metadata: Map<String, Any> = emptyMap()
-)
