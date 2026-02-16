2870360665bdc349bf087efb2e85758824a05e04
1winhyun <jimking1@naver.com>
2026-02-16 15:58:06 +0900

    test: domain/media 내 exception 테스트 코드 작성

--- src/main/java/leets/leenk/domain/media/application/exception/MediaNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package leets.leenk.domain.media.application.exception;
-
-import leets.leenk.global.common.exception.BaseException;
-
-public class MediaNotFoundException extends BaseException {
-    public MediaNotFoundException() {
-        super(MediaErrorCode.MEDIA_NOT_FOUND);
-    }
-}
